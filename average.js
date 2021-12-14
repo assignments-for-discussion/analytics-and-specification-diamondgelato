9428ba72af4d42e47fa34ee18f0ef14896b03cb0
Mugdha Kurkure <60062557+diamondgelato@users.noreply.github.com>
2021-12-14 09:21:16 +0530

    fix: ignore NaN in input

--- average.js
+++ average.js
@@ -1,6 +1,6 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  return numbers.filter((element) => element != NaN).reduce((p, c)=> p + c, 0) / numbers.length;
 }
 
 module.exports = {average};
